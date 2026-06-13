6a14583e079b4e3f099ee38e86c97af3ac928546
vinit <vinitrabadiyasoft.en@gmail.com>
2026-06-13 14:56:56 +0300

    implement routing in App component with Home, Dashboard, and History routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
-function App() {}
+function App() {
+  <Routes>
+    <Route index element={<Home />} />
+    <Route path="/dashboard/:analysisId" element={<Dashboard />} />
+    <Route path="/History" element={<History />} />
+  </Routes>;
+}
 
 export default App;
